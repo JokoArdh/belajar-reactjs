d0b161e8e05e439e308f10f6a25d03c1157e0abd
JokoArdh <c20010007@uby.ac.id>
2023-08-21 04:14:31 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -12,32 +12,46 @@ function App() {
 
   const [blogs, setBlogs] = useState([]);
 
+  const [Loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
   const handleDelete = (id) => {
     // console.log(id);
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
 
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("http://localhost:8000/blogs");
         console.log(response.data);
+        setBlogs(response.data);
+        setLoading(false);
+        setError(false);
       } catch (error) {
         console.log(error);
+        setError(true);
+        setLoading(false);
       }
     };
-    fetchData();
-  });
 
+    setTimeout(() => {
+      fetchData();
+    }, 2000);
+   
+  }, []);
 
     return (
       <div>
 
-      <Navbar brand='Halo Blog' />
+      <Navbar brand='Halo Blog ' />
 
         <div className="container mt-5">
-        <h1 className='text-danger'>Halo Dunia</h1>
+        <h1 className='text-danger'>Halo Dunia </h1>
+        {error && <h1 className='text-danger'>Eror Fetch data !</h1>}
+        {Loading && <h1 className='text-dark'>Loading ...</h1>}
         <ItemBlog blogs={blogs} handleDelete={handleDelete} />
       </div>
       </div>
